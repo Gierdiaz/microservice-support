b4baf2506373f424995df92b37f7a94345072e06
Gierdiaz <Gierdiaz@hotmail.com>
2024-12-10 21:12:45 -0300

    mudança na estrutura

--- internal/domain/interfaces/correios_interface.go
+++
@@ -1,8 +0,0 @@
-package interfaces
-
-import entities "github.com/Gierdiaz/internal/domain/entities"
-
-type CorreiosInterface interface {
-	CalcularFrete(frete *entities.Frete) (*entities.Frete, error)
-	ConsultarPrazo(cepOrigem, cepDestino string) (int, error)
-}
--- internal/endpoints/router.go
+++ internal/endpoints/router.go
@@ -3,7 +3,6 @@ package endpoints
 import (
 	"github.com/Gierdiaz/config"
 	"github.com/Gierdiaz/internal/handlers"
-	"github.com/Gierdiaz/internal/integrations"
 	"github.com/Gierdiaz/pkg/logger"
 	"github.com/gin-gonic/gin"
 )
@@ -11,15 +10,8 @@ import (
 func SetupRouter(config *config.Config) *gin.Engine {
 	r := gin.Default()
 
-	correiosIntegration := integrations.NewCorreiosIntegration()
-
 	SupportHandler := handlers.NewSupportHandler()
 	r.GET("/support", SupportHandler.GetSupport)
-
-	correios := r.Group("/correios")
-	{
-		correios.POST("/frete/calcular", correiosIntegration.FreteHandler.CalcularFrete)
-	}
 
 	logger.Logger.Info("Servidor iniciado na porta 8080")
 
